d53d056449e82fdc4a83cf30a212cc511b3a5e6b
Eashin Matubber <eashin@gmail.com>
2019-10-01 19:41:14 +0300

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -53,6 +53,38 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(204).end()
   })
 
+  const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+      if(!body.number) {
+          return response.status(400).json({
+              error: 'number missing'
+          })
+      }
+    }
+  
+    const person = {
+      name: body.name,
+      number:body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+
 const port = 3001;
 app.listen(port);
 console.log(`Server running on port ${port}`);
