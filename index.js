cb06c97a2958d57ca12c779aa8e66bac41d0b41c
Eashin Matubber <eashin@gmail.com>
2019-10-01 17:06:04 +0300

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 let persons = [
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  },
-  {
-    name: "Eashin Matubber",
-    number: "+358404141200",
-    id: 5
-  },
-  {
-    name: "again",
-    number: "01111",
-    id: 6
-  }
+    { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      }
 ];
 
 app.get("/info", (req, res) => {
@@ -35,6 +35,17 @@ app.get("/info", (req, res) => {
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  
+  })
 
 const port = 3001;
 app.listen(port);
