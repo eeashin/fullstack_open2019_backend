c8e3b2e92a5e427e5b421cb933cff04a0bad0775
Eashin Matubber <eashin@gmail.com>
2019-10-01 16:54:51 +0300

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
+let persons = [
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  },
+  {
+    name: "Eashin Matubber",
+    number: "+358404141200",
+    id: 5
+  },
+  {
+    name: "again",
+    number: "01111",
+    id: 6
+  }
+];
 
-    let persons = [
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      },
-      {
-        "name": "Eashin Matubber",
-        "number": "+358404141200",
-        "id": 5
-      },
-      {
-        "name": "again",
-        "number": "01111",
-        "id": 6
-      }
-    ]
+app.get("/info", (req, res) => {
+  let date = new Date();
+  let message = `Phonebook has info for ${persons.length} people.`;
+  res.send(`${date}<br/>${message}`);
+});
 
-    app.get('/', (req, res) => {
-        res.send('<h1>Hello World!</h1>')
-      })
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
-      app.get('/api/persons', (req, res) => {
-        res.json(persons)
-      })
-
-      
-
-      const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
-  
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}`);
